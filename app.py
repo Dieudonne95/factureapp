300b132b3260b0f559da517ad703a59845234c47
Dieu-donné <152850431+Dieudonne95@users.noreply.github.com>
2025-02-09 15:51:13 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px  # Ajout de Plotly pour les graphiques interactifs
 from fpdf import FPDF
 import sqlite3
